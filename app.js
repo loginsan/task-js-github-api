69adcbd67a3e6d3a8f2dd7e750be3f787bb4904d
Ilya Loginov <linsykt@gmail.com>
2020-11-06 00:46:55 +0300

    add styles

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const searchField = document.querySelector(".search");
+const hintsList = document.querySelector(".hints");
+const resultsList = document.querySelector(".results");
 
 async function getRepositories(request) {
     const qParam = 'loginsan'; //request.split(` `).join(`+`);
@@ -17,22 +20,11 @@ async function render(request = '') {
 const requestGitHub = (q) => {
   const requestURL = `https://api.github.com/search/repositories?q=${q}&per_page=5`;
   console.log(requestURL);
-  const APItoken = '2c7c5192fcc473d0275c5b75c60f4710bcc8696c';
 
-  fetch(requestURL/*, {
-      headers: {
-        Accept: 'application/vnd.github.mercy-preview+json',
-        Origin: 'null'
-      }
-    }*/)
+  fetch(requestURL)
     .then(response => console.log(response))
     .catch(error => console.log(error));
 
-  /*fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))*/
 };
 
 requestGitHub("loginsan");
-//getRepositories("loginsan");
-//render();
